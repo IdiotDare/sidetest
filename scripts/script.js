9eac16a27dafd23c448eb062e9844f0af7e500d6
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-24 17:41:00 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
 var canvas = document.querySelector('canvas');
 
-canvas.width = 1000px; //makes canvas expand to full width, excluding the margin
-canvas.height = 200px;
+canvas.width = 100%; //makes canvas expand to full width, excluding the margin
+canvas.height = 100%;
